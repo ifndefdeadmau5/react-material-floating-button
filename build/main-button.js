849ac073bdcf4d399a81354ac99b4811d507b058
Trevor <ifndefdeadmau5@gmail.com>
2017-04-19 10:55:40 +0900

    Add builded css

--- build/main-button.js
+++ build/main-button.js
@@ -19,19 +19,20 @@ var MainButton = React.createClass({
     var iconResting = classnames("mfb-component__main-icon--resting", this.props.iconResting);
     var iconActive = classnames("mfb-component__main-icon--active", this.props.iconActive);
     var mainClass = classnames("mfb-component__button--main", this.props.className);
+    var Icon = this.props.iconButton;
     if (this.props.label) {
       return React.createElement(
         "a",
         { href: this.props.href, className: mainClass, onClick: this.props.onClick, "data-mfb-label": this.props.label },
-        React.createElement("i", { className: iconResting }),
-        React.createElement("i", { className: iconActive })
+        React.createElement(Icon, { className: iconResting }),
+        React.createElement(Icon, { className: iconActive })
       );
     } else {
       return React.createElement(
         "a",
         { href: this.props.href, className: mainClass, onClick: this.props.onClick },
-        React.createElement("i", { className: iconResting }),
-        React.createElement("i", { className: iconActive })
+        React.createElement(Icon, { className: iconResting }),
+        React.createElement(Icon, { className: iconActive })
       );
     }
   }
